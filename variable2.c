b83311fa4085f6982a00feecf4d81f635002b6b3
Qingchen Yuan <yuanqingchen1994@126.com>
2017-07-17 16:28:42 +0100

    添加了运行结果

--- variable2.c
+++ variable2.c
@@ -23,3 +23,15 @@ void func1(void)
   thingy++;
   printf(" thingy 为 %d ， count 为 %d\n", thingy, count);
 }
+/*
+ thingy 为 6 ， count 为 9
+ thingy 为 7 ， count 为 8
+ thingy 为 8 ， count 为 7
+ thingy 为 9 ， count 为 6
+ thingy 为 10 ， count 为 5
+ thingy 为 11 ， count 为 4
+ thingy 为 12 ， count 为 3
+ thingy 为 13 ， count 为 2
+ thingy 为 14 ， count 为 1
+ thingy 为 15 ， count 为 0
+*/
